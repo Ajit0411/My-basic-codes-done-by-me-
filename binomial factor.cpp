db4251c0ddf6effbc37e361d6fe27ccb0e6f101c
Ajit <ajiitt.rtiwari@gmail.com>
2026-06-09 16:48:00 +0530

    Add files via upload
    a simple c++ code to find binomial factor(nCr)

---
+++ binomial factor.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+int factorial(int n){
+  int fact=1;
+  for(int i=1;i<=n;i++){
+    fact=fact*i;
+  }
+  return fact;
+}
+int nCr(int n,int r){
+    return factorial(n)/factorial(r)*factorial(n-r);
+}
+int main(){
+    int n;
+    int r;
+    std::cout<<"Enter n and r: ";
+    std::cin>>n>>r;
+    std::cout<<"nCr: "<<nCr(n,r)<<std::endl;
+}
